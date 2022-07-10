eb14b3e84cf7a7c8a3f107d330b133c658f21761
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2022-07-10 15:53:30 +0200

    updated project dependencies; set version to 0.1.2

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject zcfux/confick "0.1.1"
+(defproject zcfux/confick "0.1.2"
   :description "Simple, stupid configuration management."
   :url "https://github.com/20centaurifux/confick"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.memoize "1.0.236"]
+                 [org.clojure/core.memoize "1.0.257"]
                  [environ "1.2.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :test {:env {:edn-config-path "test.edn"}}}
-  :plugins [[lein-cljfmt "0.6.7"]
+  :plugins [[lein-cljfmt "0.8.2"]
             [lein-environ "1.2.0"]
-            [lein-codox "0.10.7"]]
+            [lein-codox "0.10.8"]]
   :codox { :output-path "./doc" })
