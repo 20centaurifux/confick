6861465dc6e0c1e8c1627d1f708bc5fe04e301b2
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2024-11-06 21:59:36 +0100

    changed Clojars group; updated dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject zcfux/confick "0.1.4"
+(defproject de.dixieflatline/confick "0.1.4"
   :description "Simple, stupid configuration management."
   :url "https://github.com/20centaurifux/confick"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.memoize "1.0.257"]
+                 [org.clojure/core.memoize "1.1.266"]
                  [environ "1.2.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :test {:env {:edn-config-path "test.edn"}}}
-  :plugins [[lein-cljfmt "0.9.2"]
+  :plugins [[dev.weavejester/lein-cljfmt "0.13.0"]
             [lein-environ "1.2.0"]
             [lein-codox "0.10.8"]]
   :codox { :output-path "./doc" })
