5620f881cc51002fe848bcfe168f61b10a041dfd
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2024-11-08 18:06:32 +0100

    tests

--- test/confick/core_test.clj
+++ test/confick/core_test.clj
@@ -1,36 +1,75 @@
 (ns confick.core-test
   (:require [clojure.test :refer [deftest testing is]]
-            [confick.core :refer [bind]]))
+            [confick.core :refer [gulp lookup bind]]))
 
-(deftest test-bind
-  (testing "all keys found"
-    (bind [foo :foo
-           answer [:answer :of :everything]]
-      (is (= foo "bar"))
-      (is (= answer 42))))
+(deftest test-gulp
+  (testing "load configuration"
+    (let [m (gulp)]
+      (is (= {:foo "bar" :answer {:of {:everything 42}}}
+             m)))))
+
+(deftest test-lookup
+  (testing "key found"
+    (let [x (lookup :foo)]
+      (is (= x "bar"))))
 
   (testing "key not found"
-    (bind [v [:foo :bar]]
-      (is (nil? v))))
+    (let [x (lookup [:foo :bar])]
+      (is (nil? x))))
 
   (testing "set default value when key not found"
-    (bind [^{:default 42} foo :foo
-           ^{:default 23} bar :bar]
-      (is (= foo "bar"))
-      (is (= bar 23))))
+    (let [x (lookup [:foo :bar] :default "bar")]
+      (is (= "bar" x))))
 
   (testing "throw exception when required key not found"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (bind [^:required foobar [:foo :bar]]))))
+                 (lookup [:foo :bar] :required true))))
 
   (testing "set value if it conforms spec"
-    (bind [^{:conform string?} foo :foo]
-      (is (= foo "bar"))))
+    (let [x (lookup :foo :conform string?)]
+      (is (= "bar" x))))
 
   (testing "throw exception when value doesn't conform spec"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (bind [^{:conform int?} foo :foo]))))
+                 (lookup :foo :conform int?))))
 
   (testing "spec is validated after setting default value"
-    (bind [^{:default 23 :conform pos?} bar :bar]
-      (is (= bar 23)))))
+    (let [x (lookup :bar :default 23 :conform pos?)]
+      (is (= 23 x))
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (lookup :bar :default 23 :conform neg?))))))
+
+(deftest test-bind
+  (testing "all keys found"
+    (bind [a :foo
+           b [:answer :of :everything]]
+      (is (= "bar" a))
+      (is (= 42 b))))
+
+  (testing "key not found"
+    (bind [x [:foo :bar]]
+      (is (nil? x))))
+
+  (testing "set default value when key not found"
+    (bind [^{:default 42} a :foo
+           ^{:default 23} b :bar]
+      (is (= "bar" a))
+      (is (= 23 b))))
+
+  (testing "throw exception when required key not found"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (bind [^:required _ [:foo :bar]]))))
+
+  (testing "set value if it conforms spec"
+    (bind [^{:conform string?} x :foo]
+      (is (= "bar" x))))
+
+  (testing "throw exception when value doesn't conform spec"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (bind [^{:conform int?} _ :foo]))))
+
+  (testing "spec is validated after setting default value"
+    (bind [^{:default 23 :conform pos?} x :bar]
+      (is (= 23 x))
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (bind [^{:default 23 :conform neg?} _ :bar]))))))
